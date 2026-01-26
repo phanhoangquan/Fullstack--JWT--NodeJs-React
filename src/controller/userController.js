357ed5ddd4f04e568b431b6c74e5bf94b41c94d3
phan_hoang_quan <phanhoangquan123@gmail.com>
2026-01-26 20:08:52 +0700

    Base User CRUD - RESTful APIs

---
+++ src/controller/userController.js
@@ -0,0 +1,53 @@
+import { getAllUsers } from '../services/userApiService';
+
+export const handleRead = async (req, res) => {
+   try {
+      let data = await getAllUsers();
+      return res.status(200).json({
+         EM: data.EM,
+         EC: data.EC,
+         DT: data.DT,
+      });
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         EM: 'error from server',
+         EC: '-1',
+         DT: '',
+      });
+   }
+};
+
+export const handleCreate = async (req, res) => {
+   try {
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         EM: 'error from server',
+         EC: '-1',
+         DT: '',
+      });
+   }
+};
+export const handleUpdate = async (req, res) => {
+   try {
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         EM: 'error from server',
+         EC: '-1',
+         DT: '',
+      });
+   }
+};
+export const handleDelete = async (req, res) => {
+   try {
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+         EM: 'error from server',
+         EC: '-1',
+         DT: '',
+      });
+   }
+};
---
+++ src/services/userApiService.js
@@ -0,0 +1,50 @@
+import db from '../models/index';
+
+export const getAllUsers = async () => {
+   try {
+      let users = await db.User.findAll({
+         attributes: ['id', 'username', 'email', 'phone', 'sex'],
+         include: { model: db.Group, attributes: ['name', 'description'] },
+         raw: true,
+         nest: true,
+      });
+      console.log(users);
+      return {
+         EM: 'get data success',
+         EC: 0,
+         ED: users,
+      };
+   } catch (e) {
+      console.log(e);
+      return {
+         EM: 'something wrong with services',
+         EC: 1,
+         ED: [],
+      };
+   }
+};
+
+export const createNewUsers = async (data) => {
+   try {
+      await db.User.create({});
+   } catch (e) {}
+};
+
+export const updateUsers = async (data) => {
+   try {
+      let user = await db.User.findOne({ where: { id: data.id } });
+      if (user) {
+         //update
+      } else {
+         //not found user
+      }
+   } catch (e) {}
+};
+
+export const deleteUsers = async (id) => {
+   try {
+      await db.User.delete({
+         where: { id: id },
+      });
+   } catch (e) {}
+};
